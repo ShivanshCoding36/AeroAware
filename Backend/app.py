72889469feadcead4f2d2af62ecab558486febaa
Shivansh Mathur <101795319+ShivanshCoding36@users.noreply.github.com>
2025-10-05 22:53:46 +0530

    Update app.py

--- Backend/app.py
+++ Backend/app.py
@@ -330,7 +330,8 @@ def combined():
 # Run Server
 # -----------------------------
 if __name__ == "__main__":
-    app.run(debug=True, port=5000)
+    app.run(host="0.0.0.0", port=10000)
 
 
 # MAKE VIDEO, SUBMIT
+
